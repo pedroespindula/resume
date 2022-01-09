dd1b6e3788066b31d61064096182ffc5ec1c5269
Pedro Espíndula <joaopedro.jpse@gmail.com>
2022-01-08 23:44:42 -0300

    Add manual deployment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./constants.css";
 import App from "./components/app/Resume";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById("root"));
